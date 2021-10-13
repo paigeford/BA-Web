52333a45cb493dee982799df8794d22142d637b7
Paige Ford <paige.ford@bigassfans.com>
2021-10-13 08:29:44 -0400

    Add plugins and themes

--- wp-config.php
+++ wp-config.php
@@ -20,6 +20,8 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+define('WP_CACHE', true);
+define( 'WPCACHEHOME', '/Users/pford/Documents/sites/BA-Web/wp-content/plugins/wp-super-cache/' );
 define( 'DB_NAME', 'ba-web' );
 
 /** MySQL database username */
